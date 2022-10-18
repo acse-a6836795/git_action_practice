704dff93d7fe29dd5adcc83b75432c54938eda2d
Dingo Luo <luo1ding111@gmail.com>
2022-10-18 11:42:51 +0100

    gramma fixed

--- simple_functions/functions1.py
+++ simple_functions/functions1.py
@@ -1,7 +1,6 @@
-
 from functools import cache
 
-__all__ = ['my_sum', 'factorial']
+__all__ = ["my_sum", "factorial"]
 
 
 def my_sum(iterable):
@@ -13,4 +12,4 @@ def my_sum(iterable):
 
 @cache
 def factorial(n):
-    return n * factorial(n-1) if n else 1
+    return n * factorial(n - 1) if n else 1
